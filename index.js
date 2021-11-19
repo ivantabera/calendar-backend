bb6411fa7c745b55184e3539ec2b75aae0acfb92
ivantabera <ivantabera19@gmail.com>
2021-11-19 15:30:35 -0600

    Se configuro Express, para el backend server

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+const express = require('express');
 
-console.log('hola mundo!!!');
+//Crear el servidor de express
+const app = express();
+
+//Rutas
+app.get('/', (req, res) => {
+
+    res.json({
+        ok:true
+    });
+    
+});
+
+//Escuchar peticiones
+app.listen( 4000, () => {
+    console.log(`servidor corriendo en puerto ${4000}`);
+})
