1879715ca9942fc00f86f16c5e87e98ec70b0a27
ivantabera <ivantabera19@gmail.com>
2021-11-22 13:10:44 -0600

    Se generaron las rutas para los usuarios

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const express = require('express');
+require('dotenv').config();
+
+/* La siguiente consola es para buscar la propiedad PORT y se pueda trabajar sobre el puerto que nos manden */
+// console.log(process.env);
+/* Puerto en el que se va a trabajar */
+const port = process.env.PORT
 
 //Crear el servidor de express
 const app = express();
 
-//Rutas
-app.get('/', (req, res) => {
+// Mostrar index del directorio publico 
+app.use( express.static('public'));
 
-    res.json({
-        ok:true
-    });
-    
-});
+// Rutas
+// TODO auth / crear, login, renewToken
+app.use( '/api/auth', require('./routes/auth') );
+// TODO CRUD: eventos
 
 //Escuchar peticiones
-app.listen( 4000, () => {
-    console.log(`servidor corriendo en puerto ${4000}`);
+app.listen( port , () => {
+    console.log(`servidor corriendo en puerto ${port}`);
 })
