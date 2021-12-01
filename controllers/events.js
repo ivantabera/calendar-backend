2ba2266dfc2c097e49286b1d4c588b8d6d2349d6
Ivan Tabera <ivantabera19@gmail.com>
2021-12-01 12:00:24 -0600

    Actualizar Evento en la BD

--- controllers/events.js
+++ controllers/events.js
@@ -13,7 +13,6 @@ const getEventos =  async( req, res = response ) =>{
 };
 
 const createEvento = ( req, res = response ) => {
-    console.log('res', res)
     
     const evento = new Evento( req.body );
 
@@ -40,12 +39,64 @@ const createEvento = ( req, res = response ) => {
 
 };
 
-const updateEvento = ( req, res = response ) => {
+const updateEvento = async( req, res = response ) => {
 
-    res.json({
-        ok:true,
-        msg:'Evento actualizado con éxito'
-    });
+    // Varible para extraer los parametros de la url (peticion)
+    const eventoId = req.params.id;
+    // Variable para extraer de la peticion el uid del usuario 
+    const uid = req.uid;
+
+    try {
+        
+        // Se guarda en la variable el evento buscado por id
+        const evento = await Evento.findById( eventoId );
+
+        // Validar si el evento no existe
+        if( !evento ) {
+            res.status(400).json({
+                ok:false,
+                msg:'Error, el evento no existe'
+            })
+        }
+
+        // Extraemos en la variable con el metodo toString  que Devuelve una cadena que representa al objeto
+        const eventoValidacion = evento.user.toString();
+        // Validar si el usuario tiene privilegios
+        if( eventoValidacion !== uid ){
+            res.status(401).json({
+                ok:false,
+                msg:'Error, el usuario no tiene privilegios para realizar esta operacion'
+            })
+        }
+
+        // Si las validaciones pasan vamos a armar el evento para actualizarlo con el body de la solicitud
+        // y enviando el uid del usuario
+        const nuevoEvento = {
+            ...req.body,
+            user:uid
+        }
+
+        // Actualizamos el evento con el id del evento, el nuevo evento armado y el ultimo parametro
+        // nos sirve para actualizar de forma inmediata la respuesta del postman
+        const eventoActualizado = await Evento.findByIdAndUpdate( eventoId, nuevoEvento, {new:true}); 
+        
+        // Respondemos con el evento ya actualizado
+        res.json({
+            ok:true,
+            eventoActualizado
+        });
+
+    } catch (error) {
+
+        // Control del error si la peticion falla
+        console.log('error', error)
+        res.status(500).json({
+            ok:false,
+            msg:'Error, contacta al administrador'
+        })
+        
+    }
+
 
 };
 
